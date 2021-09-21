f8a7a682ebd28277ac49b7bfd40d07d9f1e323a5
Mikhail Fedotov <fedotov@rutoken.ru>
2021-09-22 00:44:05 +0300

    Fix a bug when view (button) is destroyed while a user is holding it

--- app/src/main/java/ru/nsk/kstatemachinesample/ui/main/MainFragment.kt
+++ app/src/main/java/ru/nsk/kstatemachinesample/ui/main/MainFragment.kt
@@ -3,13 +3,18 @@ package ru.nsk.kstatemachinesample.ui.main
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.nsk.kstatemachinesample.R
 import ru.nsk.kstatemachinesample.databinding.MainFragmentBinding
@@ -30,14 +35,16 @@ class MainFragment : Fragment() {
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.logTextView.post { binding.logTextView.scrollToBottom() }
+
         binding.duckButton.setOnTouchListener(
-            TouchListener(onDown = { DuckPressEvent.send() }, onUp = { DuckReleaseEvent.send() })
+            TouchListener(lifecycle, onDown = { DuckPressEvent.send() }, onUp = { DuckReleaseEvent.send() })
         )
         binding.fireButton.setOnTouchListener(
-            TouchListener(onDown = { FirePressEvent.send() }, onUp = { FireReleaseEvent.send() })
+            TouchListener(lifecycle, onDown = { FirePressEvent.send() }, onUp = { FireReleaseEvent.send() })
         )
         binding.jumpButton.setOnTouchListener(
-            TouchListener(onDown = { JumpPressEvent.send() }, onUp = { /* empty */ })
+            TouchListener(lifecycle, onDown = { JumpPressEvent.send() }, onUp = { /* empty */ })
         )
 
         viewModel.controlEventChanged.observe(viewLifecycleOwner) {
@@ -68,26 +75,47 @@ class MainFragment : Fragment() {
 
     private fun log(text: String) = with(binding.logTextView) {
         append(text + lineSeparator())
-        // scroll to bottom
-        layout?.let { layout ->
-            val scrollDelta = (layout.getLineBottom(lineCount - 1) - scrollY - height)
-            if (scrollDelta > 0) scrollBy(0, scrollDelta)
-        }
+        scrollToBottom()
     }
 
     private fun ControlEvent.send() = viewModel.sendEvent(this)
 }
 
+private fun TextView.scrollToBottom() {
+    layout?.let { layout ->
+        val scrollDelta = (layout.getLineBottom(lineCount - 1) - scrollY - height)
+        if (scrollDelta > 0) scrollBy(0, scrollDelta)
+    }
+}
+
 private inline fun <reified S : HeroState> List<HeroState>.hasState() = filterIsInstance<S>().isNotEmpty()
 
-private class TouchListener(private val onDown: () -> Unit, private val onUp: () -> Unit) : View.OnTouchListener {
+private class TouchListener(lifecycle: Lifecycle, private val onDown: () -> Unit, private val onUp: () -> Unit) :
+    View.OnTouchListener, DefaultLifecycleObserver {
+    private var holding = false
+
+    init {
+        lifecycle.addObserver(this)
+    }
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouch(v: View, event: MotionEvent): Boolean {
         when (event.action) {
-            MotionEvent.ACTION_DOWN -> onDown()
-            MotionEvent.ACTION_UP -> onUp()
+            MotionEvent.ACTION_DOWN ->  {
+                holding = true
+                onDown()
+            }
+            MotionEvent.ACTION_UP -> {
+                holding = false
+                onUp()
+            }
             else -> return false
         }
         return true
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        // send release event if a view is destroying while a user holds it
+        if (holding) onUp()
     }
 }
