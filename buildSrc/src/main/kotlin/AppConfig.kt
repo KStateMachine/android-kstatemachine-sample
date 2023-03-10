5f1b88589f02975d323d9b2d8e23e6a182d58dce
Mikhail Fedotov <MFedotov2@alfabank.ru>
2023-03-10 12:05:22 +0300

    Up version to 1.0.5

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,7 +2,7 @@ object AppConfig {
     const val compileSdk = 31
     const val minSdk = 23
     const val targetSdk = 31
-    const val versionCode = 2
-    const val versionName = "1.0.4"
+    const val versionCode = 5
+    const val versionName = "1.0.5"
     const val buildToolsVersion = "31.0.0"
 }
